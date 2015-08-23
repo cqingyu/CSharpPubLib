79ace185eaf069ed68933f5c22cc7dc633971721
cqingyu <cqingyu@gmail.com>
2015-08-23 14:33:23 +0800

    初始版本：各种方式Hash计算

---
+++ CsPubLib/FormHash.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CsPubLib
+{
+    public partial class FormHash : Form
+    {
+        public FormHash()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHashCalc_Click(object sender, EventArgs e)
+        {
+            //Console.WriteLine("MD5 of \"abc\"");
+
+            //Console.WriteLine("MD5_32(Upper): {0}",
+            //    HashHelper.Hash_MD5_32("abc"));
+            //Console.WriteLine("MD5_32(Lower): {0}",
+            //    HashHelper.Hash_MD5_32("abc", false));
+
+            //Console.WriteLine("MD5_16(Upper): {0}",
+            //    HashHelper.Hash_MD5_16("abc"));
+            //Console.WriteLine("MD5_16(Lower): {0}",
+            //    HashHelper.Hash_MD5_16("abc", false));
+
+            //Console.WriteLine("2_MD5_32(Upper): {0}",
+            //    HashHelper.Hash_2_MD5_32("abc"));
+            //Console.WriteLine("2_MD5_32(Lower): {0}",
+            //    HashHelper.Hash_2_MD5_32("abc", false));
+
+            //Console.WriteLine("2_MD5_32(Upper): {0}",
+            //    HashHelper.Hash_2_MD5_16("abc"));
+            //Console.WriteLine("2_MD5_32(Lower): {0}",
+            //    HashHelper.Hash_2_MD5_16("abc", false));
+
+            //Console.WriteLine("SHA of \"abc\"");
+
+            //Console.WriteLine("SHA-1(Upper): {0}",
+            //    HashHelper.Hash_SHA_1("abc"));
+            //Console.WriteLine("SHA-1(Lower): {0}",
+            //    HashHelper.Hash_SHA_1("abc", false));
+
+            //Console.WriteLine("SHA-256(Upper): {0}",
+            //    HashHelper.Hash_SHA_256("abc"));
+            //Console.WriteLine("SHA-256(Lower): {0}",
+            //    HashHelper.Hash_SHA_256("abc", false));
+
+            //Console.WriteLine("SHA-384(Upper): {0}",
+            //    HashHelper.Hash_SHA_384("abc"));
+            //Console.WriteLine("SHA-384(Lower): {0}",
+            //    HashHelper.Hash_SHA_384("abc", false));
+
+            //Console.WriteLine("SHA-512(Upper): {0}",
+            //    HashHelper.Hash_SHA_512("abc"));
+            //Console.WriteLine("SHA-512(Lower): {0}",
+            //    HashHelper.Hash_SHA_512("abc", false));
+
+            //Console.ReadLine();
+        }
+    }
+}
